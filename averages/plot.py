fc9fe87e21b55759454038081cb6503e8c39df80
unknown <kuksenko.dima@gmail.com>
2022-09-20 09:19:09 -0600

    added plot py to plot averages

---
+++ averages/plot.py
@@ -0,0 +1,16 @@
+"""
+Plots volume averages vs. pseudo time increments. 
+
+Author: Dmytro Kuksenko
+Date: Sept 20, 2022
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot(data):
+    """
+    Plots the volume averages vs pseudo time
+    """
+    pass
